f9732a7f68b4e4cacb126c6d7450941d147a0b0f
Mark <msmoawad99@gmail.com>
2022-02-24 16:11:50 -0500

    fix

--- rest.py
+++ rest.py
@@ -4,6 +4,8 @@ from flask import Flask, jsonify, request
 import pika
 
 app = Flask(__name__)
+if "__main__" == __name__:
+    app.run(host="0.0.0.0", port=80)
 
 @app.route('/listen', methods=['POST'])
 def listen():
